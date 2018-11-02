a91fefa4b42def8cf6805753054880af3da1d721
Manu Gonzalez <jgonzalezcantarero@gmail.com>
2018-11-02 11:23:23 +0100

    Chapter 16

--- play.js
+++ play.js
@@ -11,5 +11,7 @@ const hobbies = ["Sports", "Cooking"];
 //     console.log(hobby);
 // }
 
-console.log(hobbies.map(hobby => "Hobby: " + hobby));
+// console.log(hobbies.map(hobby => "Hobby: " + hobby));
+// console.log(hobbies);
+hobbies.push("Programming");
 console.log(hobbies);
