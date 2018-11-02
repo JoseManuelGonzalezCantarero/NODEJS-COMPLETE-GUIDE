900446abbd3b6e19f25de53bd7d167b2ae0c8bd2
Manu Gonzalez <jgonzalezcantarero@gmail.com>
2018-11-02 11:32:34 +0100

    Chapter 17

--- play.js
+++ play.js
@@ -6,12 +6,21 @@ const person = {
     }
 };
 
+const copiedPerson = {...person};
+console.log(copiedPerson);
+
 const hobbies = ["Sports", "Cooking"];
 // for (let hobby of hobbies) {
 //     console.log(hobby);
 // }
-
 // console.log(hobbies.map(hobby => "Hobby: " + hobby));
 // console.log(hobbies);
-hobbies.push("Programming");
-console.log(hobbies);
+const copiedArray = [...hobbies];
+// hobbies.push("Programming");
+console.log(copiedArray);
+
+const toArray = (...args) => {
+    return args;
+};
+
+console.log(toArray(1, 2, 3, 4));
